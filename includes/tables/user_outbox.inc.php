6470d221243c0635174b451cec37257e60953b49
dan2 <dan2@eeac7d0b-4535-ccf8-9bcc-215df3ce543c>
2007-01-14 23:44:48 +0000

    - messages from user's mailbox are correctly shown - send/reply function corrected - send messages are saved in the outbox folder

---
+++ includes/tables/user_outbox.inc.php
@@ -0,0 +1,15 @@
+<?php
+/******************************************************************************
+newdsb
+===============================================================================
+File:                       includes/tables/user_outbox.inc.php
+$Revision: 85 $
+Software by:                DateMill (http://www.datemill.com)
+Copyright by:               DateMill (http://www.datemill.com)
+Support at:                 http://forum.datemill.com
+*******************************************************************************
+* See the "softwarelicense.txt" file for license.                             *
+******************************************************************************/
+
+$user_outbox_default['defaults']=array('mail_id'=>0,'fk_user_id'=>0,'fk_user_id_to'=>0,'_user_to'=>'','subject'=>'','message_body'=>'','date_sent'=>'','message_type'=>0);
+$user_outbox_default['types']=array('mail_id'=>_HTML_INT_,'fk_user_id'=>_HTML_INT_,'fk_user_id_to'=>_HTML_INT_,'_user_to'=>_HTML_TEXTFIELD_,'subject'=>_HTML_TEXTFIELD_,'message_body'=>_HTML_TEXTAREA_,'date_sent'=>_HTML_TEXTFIELD_,'message_type'=>_HTML_INT_);
