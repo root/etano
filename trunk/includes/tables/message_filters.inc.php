be5f869329b6c98ccf50a4d6208c2a79ccafed95
dan2 <dan2@eeac7d0b-4535-ccf8-9bcc-215df3ce543c>
2007-01-19 20:53:03 +0000

    - started the work at the filters for messages

---
+++ trunk/includes/tables/message_filters.inc.php
@@ -0,0 +1,15 @@
+<?php
+/******************************************************************************
+newdsb
+===============================================================================
+File:                       includes/tables/user_inbox.inc.php
+$Revision: 25 $
+Software by:                DateMill (http://www.datemill.com)
+Copyright by:               DateMill (http://www.datemill.com)
+Support at:                 http://forum.datemill.com
+*******************************************************************************
+* See the "softwarelicense.txt" file for license.                             *
+******************************************************************************/
+
+$message_filters_default['defaults']=array('filter_id'=>0,'filter_type'=>1,'fk_user_id'=>0,'rule'=>0,'fk_folder_id'=>0);
+$message_filters_default['types']=array('filter_id'=>_HTML_INT_,'filter_type'=>_HTML_INT_,'fk_user_id'=>_HTML_INT_,'rule'=>_HTML_INT_,'fk_folder_id'=>_HTML_INT_);
