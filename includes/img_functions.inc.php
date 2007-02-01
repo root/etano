11d199c65299f7bf5a0c893ed3dc2ae6b16c41b6
dan <dan@eeac7d0b-4535-ccf8-9bcc-215df3ce543c>
2007-02-01 13:49:24 +0000

    - admin: rounded corners option for member photos (thumbnail 1) - admin: fixed a bug in profile.html - link to photo album was displayed only if the member had a main photo - optimized the photo upload process - saved a query per uploaded photo

--- includes/img_functions.inc.php
+++ includes/img_functions.inc.php
@@ -11,7 +11,7 @@ Support at:                 http://forum.datemill.com
 * See the "softwarelicense.txt" file for license.                             *
 ******************************************************************************/
 
-function save_thumbnail($image,$size,$save_path,$save_name) {
+function save_thumbnail($image,$size,$save_path,$save_name,$config=array()) {
 	$myreturn=false;
 	if ($imginfo=getimagesize($image)) {
 		$orig_size=array($imginfo[0],$imginfo[1]);
@@ -51,24 +51,44 @@ function save_thumbnail($image,$size,$save_path,$save_name) {
 			}
 		}
 
-		$watermark=get_site_option(array('watermark_text','watermark_text_color'),'core_photo');
-		if (!empty($watermark['watermark_text'])) {
-			$watermark['watermark_text_color']=str_pad($watermark['watermark_text_color'],6,'0',STR_PAD_RIGHT);
-			$text_color=imagecolorallocate($mynewimg,hexdec(substr($watermark['watermark_text_color'],0,2)),hexdec(substr($watermark['watermark_text_color'],2,2)),hexdec(substr($watermark['watermark_text_color'],4,2)));
-			$text_color2=imagecolorallocate($mynewimg,255-hexdec(substr($watermark['watermark_text_color'],0,2)),255-hexdec(substr($watermark['watermark_text_color'],2,2)),255-hexdec(substr($watermark['watermark_text_color'],4,2)));
+		if (!empty($config['watermark_text'])) {
+			$config['watermark_text_color']=str_pad($config['watermark_text_color'],6,'0',STR_PAD_RIGHT);
+			$text_color=imagecolorallocate($mynewimg,hexdec(substr($config['watermark_text_color'],0,2)),hexdec(substr($config['watermark_text_color'],2,2)),hexdec(substr($config['watermark_text_color'],4,2)));
+			$text_color2=imagecolorallocate($mynewimg,255-hexdec(substr($config['watermark_text_color'],0,2)),255-hexdec(substr($config['watermark_text_color'],2,2)),255-hexdec(substr($config['watermark_text_color'],4,2)));
 			$font_size=15;
 			do {
 				--$font_size;
-				$text_box=imagettfbbox($font_size,0,_BASEPATH_.'/includes/fonts/verdana.ttf',$watermark['watermark_text']);
+				$text_box=imagettfbbox($font_size,0,_BASEPATH_.'/includes/fonts/verdana.ttf',$config['watermark_text']);
 				$textlen=$text_box[2]-$text_box[0]+5;
 			} while ($textlen>$size);
 			$watermark_x=(int)(($size-$new_size[0])/2)+5;
 			$watermark_y=$new_size[1]+(int)(($size-$new_size[1])/2)-20;
 			//shadow first
-			ImageTTFText($mynewimg,$font_size,0,$watermark_x,$watermark_y,$text_color2,_BASEPATH_.'/includes/fonts/verdana.ttf',$watermark['watermark_text']);
+			ImageTTFText($mynewimg,$font_size,0,$watermark_x,$watermark_y,$text_color2,_BASEPATH_.'/includes/fonts/verdana.ttf',$config['watermark_text']);
 			//text second
-			ImageTTFText($mynewimg,$font_size,0,$watermark_x+1,$watermark_y+1,$text_color,_BASEPATH_.'/includes/fonts/verdana.ttf',$watermark['watermark_text']);
+			ImageTTFText($mynewimg,$font_size,0,$watermark_x+1,$watermark_y+1,$text_color,_BASEPATH_.'/includes/fonts/verdana.ttf',$config['watermark_text']);
 		}
+
+		if (!empty($config['round_corners'])) {
+			$skin=get_my_skin();
+			imagealphablending($mynewimg,true);
+			// put the corners
+			$corner=@imagecreatefrompng(_BASEPATH_.'/skins/'.$skin.'/images/corner_tl.png');
+			imagecopy($mynewimg,$corner,0,0,0,0,7,7);
+			$corner=@imagecreatefrompng(_BASEPATH_.'/skins/'.$skin.'/images/corner_tr.png');
+			imagecopy($mynewimg,$corner,$size-7,0,0,0,7,7);
+			$corner=@imagecreatefrompng(_BASEPATH_.'/skins/'.$skin.'/images/corner_bl.png');
+			imagecopy($mynewimg,$corner,0,$size-7,0,0,7,7);
+			$corner=@imagecreatefrompng(_BASEPATH_.'/skins/'.$skin.'/images/corner_br.png');
+			imagecopy($mynewimg,$corner,$size-7,$size-7,0,0,7,7);
+			// draw the border lines
+			$border_color=imagecolorallocate($mynewimg,0xCC,0xCC,0xCC);
+			imageline($mynewimg,7,0,$size-8,0,$border_color);				//tl->tr
+			imageline($mynewimg,$size-1,7,$size-1,$size-8,$border_color);	//tr->br
+			imageline($mynewimg,7,$size-1,$size-8,$size-1,$border_color);	//bl->br
+			imageline($mynewimg,0,7,0,$size-8,$border_color);				//tl->bl
+		}
+
 		imagejpeg($mynewimg,$save_path.'/'.$save_name.'.jpg',100);
 		$myreturn=true;
 	}
