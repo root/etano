60ff90cf4af0d3723ea4d3c7b42e197c746f4562
dan2 <dan2@eeac7d0b-4535-ccf8-9bcc-215df3ce543c>
2007-02-11 18:57:04 +0000

    - made the "send a flirt" option

---
+++ flirt_send_left.php
@@ -0,0 +1,25 @@
+<?php
+/******************************************************************************
+newdsb
+===============================================================================
+File:                       flirt_send_left.php
+$Revision: 51 $
+Software by:                DateMill (http://www.datemill.com)
+Copyright by:               DateMill (http://www.datemill.com)
+Support at:                 http://forum.datemill.com
+*******************************************************************************
+* See the "softwarelicense.txt" file for license.                             *
+******************************************************************************/
+
+$user_friends=array();
+
+$query="SELECT * FROM `{$dbtable_prefix}message_filters` WHERE `fk_user_id`='".$_SESSION['user']['user_id']."' AND `filter_type`='"._FILTER_USER_."' AND `field_value`='".$uid."' AND `fk_folder_id`='"._FOLDER_SPAMBOX_."'";
+if (!($res=@mysql_query($query))) {trigger_error(mysql_error(),E_USER_ERROR);}
+if (mysql_num_rows($res)) {
+	$tpl->set_var('unblock_user',true);
+}
+
+$tpl->set_file('left_content','flirt_send_left.html');
+$tpl->set_loop('user_friends',$user_friends);
+$tpl->process('left_content','left_content',TPL_LOOP | TPL_NOLOOP | TPL_OPTIONAL);
+?>
