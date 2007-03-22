a9c1b1394b85a527d7126655aa8473833540d7dc
dan <dan@eeac7d0b-4535-ccf8-9bcc-215df3ce543c>
2007-03-22 19:14:39 +0000

    - fixed a bug with the thumbnailer function when the photo was smaller than the needed width. - added the "view all in this network" functionality to search.php - finished view in gallery/list mode for search results. Works with or without javascript.

--- js/search.js
+++ js/search.js
@@ -1,10 +1,12 @@
 $(function() {
 	$('#gview_switch').bind('click',function() {
 		$('ul.table_row').removeClass('list_view').addClass('gallery_view');
+		createCookie('sco_app[rv_mode]','g',365);	// results_view_mode
 		return false;
 	});
 	$('#lview_switch').bind('click',function() {
 		$('ul.table_row').removeClass('gallery_view').addClass('list_view');
+		createCookie('sco_app[rv_mode]','l',365);	// results_view_mode
 		return false;
 	});
 });
