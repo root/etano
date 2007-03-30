c75338f2d3eea43278eec79ad02e706f7a75b643
dan <dan@eeac7d0b-4535-ccf8-9bcc-215df3ce543c>
2007-03-30 08:03:26 +0000

    - added the preferences page for members - added first/last page for the pager. - made the site_options work with the existing HTML_* constants - added HTML_INT to the option_type of site_options tables.

--- processors/preferences.php
+++ processors/preferences.php
@@ -2,8 +2,8 @@
 /******************************************************************************
 newdsb
 ===============================================================================
-File:                       admin/processors/site_options.php
-$Revision$
+File:                       processors/preferences.php
+$Revision: 67 $
 Software by:                DateMill (http://www.datemill.com)
 Copyright by:               DateMill (http://www.datemill.com)
 Support at:                 http://forum.datemill.com
@@ -11,12 +11,12 @@ Support at:                 http://forum.datemill.com
 * See the "softwarelicense.txt" file for license.                             *
 ******************************************************************************/
 
-require_once '../../includes/sessions.inc.php';
-require_once '../../includes/classes/phemplate.class.php';
-require_once '../../includes/vars.inc.php';
-require_once '../../includes/admin_functions.inc.php';
+require_once '../includes/sessions.inc.php';
+require_once '../includes/classes/phemplate.class.php';
+require_once '../includes/user_functions.inc.php';
+require_once '../includes/vars.inc.php';
 db_connect(_DBHOSTNAME_,_DBUSERNAME_,_DBPASSWORD_,_DBNAME_);
-allow_dept(DEPT_ADMIN);
+check_login_member(3);
 
 $error=false;
 $qs='';
@@ -24,10 +24,12 @@ $qs_sep='';
 $topass=array();
 if ($_SERVER['REQUEST_METHOD']=='POST') {
 	$input=array();
+	$types=array();
 
-	$query="SELECT `config_option`,`option_type`,`fk_module_code` FROM `{$dbtable_prefix}site_options3`";
+	$query="SELECT `config_option`,`option_type`,`fk_module_code` FROM `{$dbtable_prefix}site_options3` WHERE `per_user`=1";
 	if (!($res=@mysql_query($query))) {trigger_error(mysql_error(),E_USER_ERROR);}
 	while ($rsrow=mysql_fetch_assoc($res)) {
+		$types[$rsrow['fk_module_code']][$rsrow['config_option']]=$rsrow['option_type'];
 		switch ($rsrow['option_type']) {
 
 			case HTML_CHECKBOX:
@@ -51,16 +53,16 @@ if ($_SERVER['REQUEST_METHOD']=='POST') {
 
 	foreach ($input as $module_code=>$v) {
 		foreach ($v as $config_option=>$config_value) {
-			$query="UPDATE `{$dbtable_prefix}site_options3` SET `config_value`='$config_value' WHERE `config_option`='$config_option' AND `fk_module_code`='$module_code'";
-			if (!($res=@mysql_query($query))) {trigger_error(mysql_error(),E_USER_ERROR);}
+			// with this if() we target date_format because an empty date_format
+			// could break all dates on the site.
+			if ($types[$module_code][$config_option]!=HTML_TEXTFIELD || !empty($config_value)) {
+				$query="REPLACE INTO `{$dbtable_prefix}user_settings2` SET `fk_user_id`='".$_SESSION['user']['user_id']."',`config_option`='$config_option',`config_value`='$config_value',`fk_module_code`='$module_code'";
+				if (!($res=@mysql_query($query))) {trigger_error(mysql_error(),E_USER_ERROR);}
+			}
 		}
 	}
 	$topass['message']['type']=MESSAGE_INFO;
-	$topass['message']['text']='Site features updated.';
-
-	if (isset($_POST['module_code'])) {
-		$qs.=$qs_sep.'module_code='.$_POST['module_code'];
-	}
+	$topass['message']['text']='Your preferences were saved.';
 }
-redirect2page('admin/site_options.php',$topass,$qs);
+redirect2page('preferences.php',$topass,$qs);
 ?>
