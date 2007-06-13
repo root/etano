a884d356a8c43d671aae7ac6258367c19c88fe26
dan <dan@eeac7d0b-4535-ccf8-9bcc-215df3ce543c>
2007-06-13 11:53:49 +0000

    - the pager is now compatible with mod_rewrite.php - more links fixed to work with rewrite

--- mod_rewrite.php
+++ mod_rewrite.php
@@ -15,27 +15,26 @@ require_once 'includes/sessions.inc.php';
 require_once 'includes/vars.inc.php';
 
 $request_uri=$_SERVER['REQUEST_URI'];
+if (($pos=strpos($request_uri,'?'))!==false) {
+	$request_uri=substr($request_uri,0,$pos);
+}
+//phpinfo();die;
 
 $baseurl_parts=explode('/',substr(_BASEURL_,7));
 if (($pos=strpos(_BASEURL_,'/',8))!==false) {
 	$request_uri=str_replace(substr(_BASEURL_,$pos),'',$request_uri);
 }
-if ($request_uri{0}=='/') {
-	$request_uri=substr($request_uri,1);
-}
-if ($request_uri{strlen($request_uri)-1}=='/') {
-	$request_uri=substr($request_uri,0,-1);
-}
-
-//phpinfo();
-//print $request_uri;die;
+$request_uri=trim($request_uri,'/');
 $uri_parts=explode('/',$request_uri);
 
 if ($uri_parts[0]=='blog' && isset($uri_parts[1])) {
 	$_GET['pid']=$uri_parts[1];
 	require_once 'blog_post_view.php';
-} elseif ($uri_parts[0]=='browse') {
-
+} elseif ($uri_parts[0]=='inbox') {
+	require_once 'mailbox.php';
+} elseif ($uri_parts[0]=='photo' && isset($uri_parts[1])) {
+	$_GET['photo_id']=$uri_parts[1];
+	require_once 'photo_view.php';
 } elseif (!empty($uri_parts[0]) && !isset($uri_parts[1])) {
 	$_GET['user']=$uri_parts[0];
 	require_once 'profile.php';
