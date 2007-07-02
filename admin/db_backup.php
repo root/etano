51e65dc5d0d5c9c59a233e3d21f618db38bf620b
dan <dan@eeac7d0b-4535-ccf8-9bcc-215df3ce543c>
2007-07-02 19:19:03 +0000

    - jquery library upgraded to 1.1.3 - added db_backup to admin - added an weekly cleanup cron job

--- admin/db_backup.php
+++ admin/db_backup.php
@@ -3,7 +3,7 @@
 Etano
 ===============================================================================
 File:                       admin/db_backup.php
-$Revision: 174 $
+$Revision: 186 $
 Software by:                DateMill (http://www.datemill.com)
 Copyright by:               DateMill (http://www.datemill.com)
 Support at:                 http://forum.datemill.com
@@ -14,59 +14,15 @@ Support at:                 http://forum.datemill.com
 require_once '../includes/common.inc.php';
 db_connect(_DBHOSTNAME_,_DBUSERNAME_,_DBPASSWORD_,_DBNAME_);
 require_once '../includes/admin_functions.inc.php';
+require_once '../includes/tables/admin_accounts.inc.php';
 allow_dept(DEPT_ADMIN);
 
-$query="SHOW TABLES";
-if (!($res=@mysql_query($query))) {trigger_error(mysql_error(),E_USER_ERROR);}
-$tables=array();
-while ($table=mysql_fetch_row($res)) {
-	$tables[]=$table[0];
-}
+$tpl=new phemplate('skin/','remove_nonjs');
+$tpl->set_file('content','db_backup.html');
+$tpl->process('content','content');
 
-$towrite='';
-for ($i=0;isset($tables[$i]);++$i) {
-	$towrite.="CREATE TABLE `".$tables[$i]."` (";
-	$query="SHOW COLUMNS FROM `".$tables[$i]."`";
-	if (!($res=@mysql_query($query))) {trigger_error(mysql_error(),E_USER_ERROR);}
-	while ($rsrow=mysql_fetch_assoc($res)) {
-		$towrite.="\n\t`".$rsrow['Field'].'` '.$rsrow['Type']." DEFAULT '".$rsrow['Default']."'";
-		if ($rsrow['Null']!='YES') {
-			$towrite.=' NOT NULL';
-		}
-		if (!empty($rsrow['Extra'])) {
-			$towrite.=' '.$rsrow['Extra'];
-		}
-		$towrite.=',';
-	}
-	$query="SHOW INDEXES FROM `".$tables[$i]."`";
-	if (!($res=@mysql_query($query))) {trigger_error(mysql_error(),E_USER_ERROR);}
-	$keys=array();
-	while ($rsrow=mysql_fetch_assoc($res)) {
-		if ($rsrow['Key_name']=='PRIMARY') {
-			$towrite.="\n\tPRIMARY KEY (`".$rsrow['Column_name']."`),";
-		} else {
-			if ($rsrow['Non_unique']==0) {
-				$rsrow['Non_unique']='UNIQUE KEY';
-			} elseif ($rsrow['Index_type']=='FULLTEXT') {
-				$rsrow['Non_unique']='FULLTEXT KEY';
-			} else {
-				$rsrow['Non_unique']='KEY';
-			}
-			$keys[$rsrow['Non_unique']][$rsrow['Key_name']][$rsrow['Seq_in_index']]=$rsrow['Column_name'];
-		}
-	}
-	foreach ($keys as $keytype=>$v) {
-		foreach ($v as $keyname=>$fields) {
-			$towrite.="\n\t".$keytype." `$keyname` (`".join('`,`',$fields)."`),";
-		}
-	}
-	$towrite=substr($towrite,0,-1);
-	$towrite.="\n) TYPE=";
-	$query="SHOW TABLE STATUS LIKE '".$tables[$i]."'";
-	if (!($res=@mysql_query($query))) {trigger_error(mysql_error(),E_USER_ERROR);}
-	$rsrow=mysql_fetch_assoc($res);
-	$towrite.=$rsrow['Type'].' '.$rsrow['Create_options'].";\n\n";
-}
-
-print $towrite;
+$tplvars['title']='Backup Database';
+$tplvars['page']='db_backup';
+$tplvars['css']='db_backup.css';
+include 'frame.php';
 ?>
---
+++ tools/cron/jobs/w/cleanup.php
@@ -0,0 +1,22 @@
+<?php
+$jobs[]='clean_tmp';
+
+function clean_tmp() {
+	if ($dh=opendir(_BASEPATH_.'/tmp/admin')) {
+		while (($file=readdir($dh))!==false) {
+			if ($file{0}!='.' && $file!='index.html' && is_file(_BASEPATH_.'/tmp/admin/'.$file) && filectime(_BASEPATH_.'/tmp/admin/'.$file)<mktime()-172800) {	// 2 days
+				@unlink(_BASEPATH_.'/tmp/admin/'.$file);
+			}
+		}
+		closedir($dh);
+	}
+
+	if ($dh=opendir(_BASEPATH_.'/tmp')) {
+		while (($file=readdir($dh))!==false) {
+			if ($file{0}!='.' && $file!='index.html' && is_file(_BASEPATH_.'/tmp/'.$file) && filectime(_BASEPATH_.'/tmp/'.$file)<mktime()-172800) {	// 2 days
+				@unlink(_BASEPATH_.'/tmp/'.$file);
+			}
+		}
+		closedir($dh);
+	}
+}
