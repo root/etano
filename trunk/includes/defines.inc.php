50d2ff521c84727d33c1591e15ca654079e7b8fa
dan <dan@eeac7d0b-4535-ccf8-9bcc-215df3ce543c>
2007-07-16 09:28:04 +0000

    - small fix in default defines.inc.php

--- trunk/includes/defines.inc.php
+++ trunk/includes/defines.inc.php
@@ -1,5 +1,5 @@
 <?php
-if (empty($_SERVER['HTTP_HOST'])) {
+if (!empty($_SERVER['HTTP_HOST'])) {
 	$my_url=substr($_SERVER['PHP_SELF'],0,strrpos($_SERVER['PHP_SELF'],'/'));
 	$baseurl=((isset($_SERVER['HTTPS']) && $_SERVER['HTTPS']=='on') ? 'https://' : 'http://').$_SERVER['HTTP_HOST'].$my_url;
 	header("Location: $baseurl/install/index.php");
