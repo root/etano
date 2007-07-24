cff85c16f7f8c6155be6a9987fe402579e588205
dan <dan@eeac7d0b-4535-ccf8-9bcc-215df3ce543c>
2007-07-24 07:26:22 +0000

    - some test related to issue #1

--- includes/img_functions.inc.php
+++ includes/img_functions.inc.php
@@ -11,6 +11,9 @@ Support at:                 http://www.datemill.com/forum
 * See the "docs/licenses/etano.txt" file for license.                         *
 ******************************************************************************/
 
+define('PAD_2SIDES',1);
+define('PAD_1SIDE',2);
+define('PAD_NONE',3);
 function save_thumbnail($image,$size,$save_path,$save_name,$config=array()) {
 	$myreturn=false;
 	if ($imginfo=getimagesize($image)) {
@@ -27,70 +30,77 @@ function save_thumbnail($image,$size,$save_path,$save_name,$config=array()) {
 			$myimg=@imagecreatefrompng($image);
 		}
 
-		$new_size=array();
-		$mynewimg='';
-		if ($orig_size[$relevant_length]>$size) {	// scale down
-			$new_size[$relevant_length]=$size;
-			$new_size[1-$relevant_length]=(int)($orig_size[1-$relevant_length]*($size/$orig_size[$relevant_length]));
-			if (!empty($myimg)) {
+		if (!empty($myimg)) {
+			$new_size=array();
+			$mynewimg='';
+			if ($orig_size[$relevant_length]>$size) {	// scale down
+				$new_size[$relevant_length]=$size;
+				$new_size[1-$relevant_length]=(int)($orig_size[1-$relevant_length]*($size/$orig_size[$relevant_length]));
 				$mynewimg=@imagecreatetruecolor($size,$size);
 				imagefilledrectangle($mynewimg,0,0,$size,$size,0xFFFFFF);
 				$x=(int)(($size-$new_size[0])/2);
 				$y=(int)(($size-$new_size[1])/2);
 				imagecopyresampled($mynewimg,$myimg,$x,$y,0,0,$new_size[0],$new_size[1],$orig_size[0],$orig_size[1]);
+				$size=array($size,$size);
+			} else {	// picture is smaller than the needed size
+				if ($config['padding_type']==PAD_2SIDES) {	//pad in both directions.
+					$new_size=$orig_size;
+					$size=array($size,$size);
+				} elseif ($config['padding_type']==PAD_1SIDE) {	// padding in one direction only
+					$size=$orig_size[$relevant_length];
+					$size=array($size,$size);
+					$new_size=$orig_size;
+				} else {	// padding only in one direction
+					$size=$orig_size;
+				}
+				$mynewimg=@imagecreatetruecolor($size[0],$size[1]);
+				imagefilledrectangle($mynewimg,0,0,$size[0],$size[1],0xFFFFFF);
+				$x=(int)(($size[0]-$new_size[0])/2);
+				$y=(int)(($size[1]-$new_size[1])/2);
+				imagecopy($mynewimg,$myimg,$x,$y,0,0,$new_size[0],$new_size[1]);
 			}
-		} else {					// just white padding here. picture is smaller than the needed size
-			if (!empty($myimg)) {
-//				$size=$orig_size[$relevant_length];
-				$new_size=$orig_size;
-				$mynewimg=@imagecreatetruecolor($size,$size);
-				imagefilledrectangle($mynewimg,0,0,$size,$size,0xFFFFFF);
-				$x=(int)(($size-$orig_size[0])/2);
-				$y=(int)(($size-$orig_size[1])/2);
-				imagecopy($mynewimg,$myimg,$x,$y,0,0,$orig_size[0],$orig_size[1]);
+
+			if (!empty($config['watermark_text']) && function_exists('imagettftext')) {
+				$config['watermark_text_color']=str_pad($config['watermark_text_color'],6,'0',STR_PAD_RIGHT);
+				$text_color=imagecolorallocate($mynewimg,hexdec(substr($config['watermark_text_color'],0,2)),hexdec(substr($config['watermark_text_color'],2,2)),hexdec(substr($config['watermark_text_color'],4,2)));
+				$text_color2=imagecolorallocate($mynewimg,255-hexdec(substr($config['watermark_text_color'],0,2)),255-hexdec(substr($config['watermark_text_color'],2,2)),255-hexdec(substr($config['watermark_text_color'],4,2)));
+				$font_size=15;
+				do {
+					--$font_size;
+					$text_box=imagettfbbox($font_size,0,_BASEPATH_.'/includes/fonts/arial.ttf',$config['watermark_text']);
+					$textlen=$text_box[2]-$text_box[0]+5;
+				} while ($textlen>$size[0]);
+				$watermark_x=(int)(($size[0]-$new_size[0])/2)+5;
+				$watermark_y=$new_size[1]+(int)(($size[1]-$new_size[1])/2)-20;
+				//shadow first
+				imagettftext($mynewimg,$font_size,0,$watermark_x,$watermark_y,$text_color2,_BASEPATH_.'/includes/fonts/arial.ttf',$config['watermark_text']);
+				//text second
+				imagettftext($mynewimg,$font_size,0,$watermark_x+1,$watermark_y+1,$text_color,_BASEPATH_.'/includes/fonts/arial.ttf',$config['watermark_text']);
 			}
-		}
 
-		if (!empty($config['watermark_text']) && function_exists('imagettftext')) {
-			$config['watermark_text_color']=str_pad($config['watermark_text_color'],6,'0',STR_PAD_RIGHT);
-			$text_color=imagecolorallocate($mynewimg,hexdec(substr($config['watermark_text_color'],0,2)),hexdec(substr($config['watermark_text_color'],2,2)),hexdec(substr($config['watermark_text_color'],4,2)));
-			$text_color2=imagecolorallocate($mynewimg,255-hexdec(substr($config['watermark_text_color'],0,2)),255-hexdec(substr($config['watermark_text_color'],2,2)),255-hexdec(substr($config['watermark_text_color'],4,2)));
-			$font_size=15;
-			do {
-				--$font_size;
-				$text_box=imagettfbbox($font_size,0,_BASEPATH_.'/includes/fonts/arial.ttf',$config['watermark_text']);
-				$textlen=$text_box[2]-$text_box[0]+5;
-			} while ($textlen>$size);
-			$watermark_x=(int)(($size-$new_size[0])/2)+5;
-			$watermark_y=$new_size[1]+(int)(($size-$new_size[1])/2)-20;
-			//shadow first
-			imagettftext($mynewimg,$font_size,0,$watermark_x,$watermark_y,$text_color2,_BASEPATH_.'/includes/fonts/arial.ttf',$config['watermark_text']);
-			//text second
-			imagettftext($mynewimg,$font_size,0,$watermark_x+1,$watermark_y+1,$text_color,_BASEPATH_.'/includes/fonts/arial.ttf',$config['watermark_text']);
-		}
+			if (!empty($config['round_corners'])) {
+				$skin=get_my_skin();
+				imagealphablending($mynewimg,true);
+				// put the corners
+				$corner=@imagecreatefrompng(_BASEPATH_.'/skins_site/'.$skin.'/images/corner_tl.png');
+				imagecopy($mynewimg,$corner,0,0,0,0,7,7);
+				$corner=@imagecreatefrompng(_BASEPATH_.'/skins_site/'.$skin.'/images/corner_tr.png');
+				imagecopy($mynewimg,$corner,$size[0]-7,0,0,0,7,7);
+				$corner=@imagecreatefrompng(_BASEPATH_.'/skins_site/'.$skin.'/images/corner_bl.png');
+				imagecopy($mynewimg,$corner,0,$size[1]-7,0,0,7,7);
+				$corner=@imagecreatefrompng(_BASEPATH_.'/skins_site/'.$skin.'/images/corner_br.png');
+				imagecopy($mynewimg,$corner,$size[0]-7,$size[1]-7,0,0,7,7);
+				// draw the border lines
+				$border_color=imagecolorallocate($mynewimg,0xCC,0xCC,0xCC);
+				imageline($mynewimg,7,0,$size[0]-8,0,$border_color);				//tl->tr
+				imageline($mynewimg,$size[0]-1,7,$size[0]-1,$size[1]-8,$border_color);	//tr->br
+				imageline($mynewimg,7,$size[1]-1,$size[0]-8,$size[1]-1,$border_color);	//bl->br
+				imageline($mynewimg,0,7,0,$size[1]-8,$border_color);				//tl->bl
+			}
 
-		if (!empty($config['round_corners'])) {
-			$skin=get_my_skin();
-			imagealphablending($mynewimg,true);
-			// put the corners
-			$corner=@imagecreatefrompng(_BASEPATH_.'/skins_site/'.$skin.'/images/corner_tl.png');
-			imagecopy($mynewimg,$corner,0,0,0,0,7,7);
-			$corner=@imagecreatefrompng(_BASEPATH_.'/skins_site/'.$skin.'/images/corner_tr.png');
-			imagecopy($mynewimg,$corner,$size-7,0,0,0,7,7);
-			$corner=@imagecreatefrompng(_BASEPATH_.'/skins_site/'.$skin.'/images/corner_bl.png');
-			imagecopy($mynewimg,$corner,0,$size-7,0,0,7,7);
-			$corner=@imagecreatefrompng(_BASEPATH_.'/skins_site/'.$skin.'/images/corner_br.png');
-			imagecopy($mynewimg,$corner,$size-7,$size-7,0,0,7,7);
-			// draw the border lines
-			$border_color=imagecolorallocate($mynewimg,0xCC,0xCC,0xCC);
-			imageline($mynewimg,7,0,$size-8,0,$border_color);				//tl->tr
-			imageline($mynewimg,$size-1,7,$size-1,$size-8,$border_color);	//tr->br
-			imageline($mynewimg,7,$size-1,$size-8,$size-1,$border_color);	//bl->br
-			imageline($mynewimg,0,7,0,$size-8,$border_color);				//tl->bl
+			imagejpeg($mynewimg,$save_path.'/'.$save_name.'.jpg',90);
+			$myreturn=true;
 		}
-
-		imagejpeg($mynewimg,$save_path.'/'.$save_name.'.jpg',100);
-		$myreturn=true;
 	}
 	return $myreturn;
 }
