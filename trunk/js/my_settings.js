0941982d419b22e1ad70741877f6af826131dda4
svn <svn@eeac7d0b-4535-ccf8-9bcc-215df3ce543c>
2007-08-29 10:11:51 +0000

    Fixed issues: 86

--- trunk/js/my_settings.js
+++ trunk/js/my_settings.js
@@ -8,4 +8,24 @@ $(function() {
 		$('#def_user_prefs_time_offset').val(diff);
 		return false;
 	});
+
+	$('#prefs_form input:visible:first').focus();
+
+	$('#passchange_form').bind('submit',function() {return check_passchange(this)});
 });
+
+
+function check_passchange(the) {
+	if (the.pass.value=='') {
+		alert('Please enter the new password');
+		the.pass.focus();
+		return false;
+	}
+	if (the.pass.value!=the.pass2.value) {
+		alert('The passwords do not match');
+		the.pass2.focus();
+		return false;
+	}
+	return true;
+}
+
