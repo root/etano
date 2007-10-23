209d389ba5f70547f4cd5b443d2252956d1d35b8
svn <svn@eeac7d0b-4535-ccf8-9bcc-215df3ce543c>
2007-10-23 10:35:41 +0000

    - upper help topics link - regenerate skin is working now.

--- admin/processors/regenerate_skin.php
+++ admin/processors/regenerate_skin.php
@@ -15,31 +15,19 @@ require_once '../../includes/common.inc.php';
 db_connect(_DBHOST_,_DBUSER_,_DBPASS_,_DBNAME_);
 require_once '../../includes/admin_functions.inc.php';
 allow_dept(DEPT_ADMIN);
+set_time_limit(0);
 
 $error=false;
 $qs='';
 $qs_sep='';
 $topass=array();
-if ($_SERVER['REQUEST_METHOD']=='POST') {
-	$skin=sanitize_and_format_gpc($_POST,'skin',TYPE_STRING,$__field2format[FIELD_TEXTFIELD],'');
+$skin=sanitize_and_format_gpc($_GET,'s',TYPE_STRING,$__field2format[FIELD_TEXTFIELD],'');
 
-// check for input errors
-	if (empty($skin)) {
-		$error=true;
-		$topass['message']['type']=MESSAGE_ERROR;
-		$topass['message']['text']='No skin specified for regeneration! Please select a skin.';
-	}
+if (!$error) {
+	regenerate_langstrings_array($skin);
+	regenerate_skin_cache($skin);
 
-	if (!$error) {
-// categories for profile.html first
-		$tpl->set_file('pwrow','static/pwrow.html');
-		$categs=array();
-		foreach ($_pfields as $i=>$v) {
-
-		}
-
-		$topass['message']['type']=MESSAGE_INFO;
-		$topass['message']['text']='All files in the \''.$skin.'\' skin were regenerated. You can start using this skin now';
-	}
+	$topass['message']['type']=MESSAGE_INFO;
+	$topass['message']['text']='All files in the skin were regenerated.';
 }
 redirect2page('admin/site_skins.php',$topass,$qs);
