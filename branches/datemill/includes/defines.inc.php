46ea301584c85162214570db71cc5feb2a7d73da
dan <dan@eeac7d0b-4535-ccf8-9bcc-215df3ce543c>
2007-11-09 12:32:17 +0000

    - datemill's defines.inc.php added as default to repo

--- branches/datemill/includes/defines.inc.php
+++ branches/datemill/includes/defines.inc.php
@@ -1,19 +1,19 @@
 <?php
 define('_DBHOST_','localhost');// mysql server host name
-define('_DBUSER_','root');// mysql database username
-define('_DBPASS_','rootpass');// mysql database password
-define('_DBNAME_','datemill_new');// mysql database name
+define('_DBUSER_','datemill_new');// mysql database username
+define('_DBPASS_','gicamica12');// mysql database password
+define('_DBNAME_','datemill');// mysql database name
 define('_SITENAME_','Datemill');// Your site name
-define('_BASEURL_','http://dating.sco.ro/dm_new');// protocol required (http:// )
-define('_BASEPATH_','/www/htdocs/dm_new');// path on server to your site
+define('_BASEURL_','http://www.datemill.com');// protocol required (http:// )
+define('_BASEPATH_','/var/www/htdocs/datemill/html');// path on server to your site
 define('_PHOTOURL_',_BASEURL_.'/media/pics');// protocol required (http:// ). URL to your member photos
 define('_PHOTOPATH_',_BASEPATH_.'/media/pics');// path on server to your member photos
 define('_CACHEPATH_',_BASEPATH_.'/cache');// path on server to the cache folder
-define('_FILEOP_MODE_','disk');
-define('_FTPHOST_','');
-define('_FTPPATH_','');
-define('_FTPUSER_','');
-define('_FTPPASS_','');
+define('_FILEOP_MODE_','ftp');
+define('_FTPHOST_','localhost');
+define('_FTPPATH_','/html/');
+define('_FTPUSER_','datemill');
+define('_FTPPASS_','terebentina');
 $dbtable_prefix='dsb_';
 define('USER_ACCOUNTS_TABLE',"{$dbtable_prefix}user_accounts");
 define('USER_ACCOUNT_ID','user_id');
@@ -22,7 +22,7 @@ define('USER_ACCOUNT_PASS','pass');
 define('PASSWORD_ENC_FUNC','md5');
 
 define('_LICENSE_KEY_','7B005E43F1817FA9232845'); // md5()=66886026c95a1da38cc3e630cc8a7d25
-define('_INTERNAL_VERSION_','1.00');
+define('_INTERNAL_VERSION_','0.94');
 
 $accepted_results_per_page=array(10=>10,5=>5,15=>15,20=>20);
 $accepted_images=array('jpg','jpeg','png');
