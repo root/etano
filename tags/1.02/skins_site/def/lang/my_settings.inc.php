00e25878dd681412b9685357264a5d2836ba17c7
svn <svn@eeac7d0b-4535-ccf8-9bcc-215df3ce543c>
2007-11-16 12:50:23 +0000

    Removed file/folder

--- tags/1.02/skins_site/def/lang/my_settings.inc.php
+++
@@ -1,5 +0,0 @@
-<?php
-$GLOBALS['_lang'][80]='Your preferences were saved.';
-$GLOBALS['_lang'][243]='My Settings';
-$GLOBALS['_lang'][260]='Please enter the new password.';
-$GLOBALS['_lang'][261]='The passwords do not match.';
