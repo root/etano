e91e7f187b5912fa1b8fdbf332724bef1ffb5acd
svn <svn@eeac7d0b-4535-ccf8-9bcc-215df3ce543c>
2007-11-18 17:12:21 +0000

    Removed file/folder

--- tags/1.04/admin/js/fckconfig.js
+++
@@ -1,9 +0,0 @@
-FCKConfig.ToolbarSets['Datemill'] = [
-	['Link','Unlink','Image','Table','Rule','SpecialChar','PageBreak','-','Bold','Italic','Underline','StrikeThrough','-','Subscript','Superscript','-','OrderedList','UnorderedList','-','Source'],
-	'/',
-	['FontFormat','FontName','FontSize'],
-	['TextColor','BGColor']
-] ;
-
-// the connector must be set to php in the default fckconfig.js when updating fckeditor
-// and also both filemanager php apps must be enabled.
