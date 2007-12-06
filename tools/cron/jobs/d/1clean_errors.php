11c30ec38b5e7cea3b5244767f912aa274e5d72d
dan <dan@eeac7d0b-4535-ccf8-9bcc-215df3ce543c>
2007-12-06 11:22:42 +0000

    - try to send less duplicate errors from customers to datemill.

--- tools/cron/jobs/d/1clean_errors.php
+++ tools/cron/jobs/d/1clean_errors.php
@@ -7,7 +7,7 @@ function clean_errors() {
 	if (function_exists('curl_init')) {
 		$config=get_site_option(array('collect_errors'),'adv_features');
 		if (!empty($config['collect_errors'])) {
-			$query="SELECT `log_id`,`module`,`error` FROM `{$dbtable_prefix}error_log` ORDER BY `log_id`";
+			$query="SELECT `log_id`,`module`,`error`,LEFT(`error`,200) as `thegroup` FROM `{$dbtable_prefix}error_log` GROUP BY `thegroup` ORDER BY `log_id`";
 			if (!($res=@mysql_query($query))) {trigger_error(mysql_error(),E_USER_ERROR);}
 			if (mysql_num_rows($res)) {
 				$ch=curl_init($GLOBALS['tplvars']['remote_site'].'/remote/ident.php?lk='.md5(_LICENSE_KEY_).'&bu='.rawurlencode(base64_encode(_BASEURL_)).'&v='.rawurlencode(_INTERNAL_VERSION_));
