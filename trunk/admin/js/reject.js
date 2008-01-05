120f3476a20afe8880fed232ba471d5dad122aeb
dan <dan@eeac7d0b-4535-ccf8-9bcc-215df3ce543c>
2008-01-05 14:52:47 +0000

    - removed fckeditor, added tinyMCE and changed all files using fckeditor to use tinymce - admin mtpls don't contain the whole html markup, only the content - admin/file_edit won't edit the files in richtext mode anymore

--- trunk/admin/js/reject.js
+++ trunk/admin/js/reject.js
@@ -1,13 +1,6 @@
 $(function() {
 //	get_reason();
-	oFCKeditor=new FCKeditor('reject_reason');
-	oFCKeditor.BasePath=document.location.pathname.substring(0,document.location.pathname.lastIndexOf('/'))+'/fckeditor/';
-	oFCKeditor.Config["CustomConfigurationsPath"] = oFCKeditor.BasePath+'../js/fckconfig.js';
-	oFCKeditor.Config['FullPage']=true ;
-	oFCKeditor.ToolbarSet='Datemill';
-	oFCKeditor.Height=500;
-	oFCKeditor.ReplaceTextarea();
-
+	$('#save_tpl_btn').bind('click',save_template);
 });
 
 function get_reason() {
@@ -34,10 +27,12 @@ function check_form(theform) {
 }
 
 function save_template() {
+	tinyMCE.triggerSave(true,true);
+	var mycontent=tinyMCE.getContent();
 	$.ajax({url:'ajax/save_admin_mtpl.php',
 			type: 'POST',
 			dataType:'html',
-			data:'amtpl_type='+$('#t').val()+'&amtpl_id='+$('#amtpl_id').val()+'&subject='+escape($('#reason_title').val())+'&message_body='+escape(FCKeditorAPI.GetInstance('reject_reason').GetXHTML()),
+			data:'amtpl_type='+$('#t').val()+'&amtpl_id='+$('#amtpl_id').val()+'&subject='+escape($('#reason_title').val())+'&message_body='+escape(mycontent),
 			success: function(data) {
 						alert(data);
 					}
