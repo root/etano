ec0c480622bd9edd64d386ed9f2682238b2bfd4d
dan <dan@eeac7d0b-4535-ccf8-9bcc-215df3ce543c>
2008-01-06 11:48:38 +0000

    Removed file/folder

--- tags/1.08/admin/js/db_backup.js
+++
@@ -1,6 +0,0 @@
-$(function() {
-	$('#backup_form').bind('submit',function() {
-		$('#btn_backup').attr({disabled:'disabled'});
-		$('<div><img src="skin/images/load_indicator.gif" /> Please wait, loading</div>').appendTo('#backup_form');
-	});
-});
--- tags/1.08/js/my_searches.js
+++
@@ -1,9 +0,0 @@
-function change_title(search_id,curr_title) {
-	new_title=prompt('Please enter the new title',unescape(decodeURI(curr_title)));
-	if (new_title && new_title!='' && new_title!=curr_title) {
-		$('#renamesearch_form')[0].search_id.value=search_id;
-		$('#renamesearch_form')[0].title.value=new_title;
-		$('#renamesearch_form')[0].submit();
-		return false;
-	}
-}
