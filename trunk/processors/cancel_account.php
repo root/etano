684adf268893b0718c629268b42816335da3c80e
dan <dan@eeac7d0b-4535-ccf8-9bcc-215df3ce543c>
2008-01-09 15:05:58 +0000

    - added the cancel account functionality. - changed the footer copyright notice and added a powered by text underneath

---
+++ trunk/processors/cancel_account.php
@@ -0,0 +1,23 @@
+<?php
+/******************************************************************************
+Etano
+===============================================================================
+File:                       processors/cancel_account.php
+$Revision: 365 $
+Software by:                DateMill (http://www.datemill.com)
+Copyright by:               DateMill (http://www.datemill.com)
+Support at:                 http://www.datemill.com/forum
+*******************************************************************************
+* See the "docs/licenses/etano.txt" file for license.                         *
+******************************************************************************/
+
+require_once '../includes/common.inc.php';
+db_connect(_DBHOST_,_DBUSER_,_DBPASS_,_DBNAME_);
+require_once '../includes/user_functions.inc.php';
+check_login_member('auth');
+
+$query="UPDATE `{$dbtable_prefix}user_profiles` SET `del`=1 WHERE `fk_user_id`=".$_SESSION[_LICENSE_KEY_]['user']['user_id'];
+if (!($res=@mysql_query($query))) {trigger_error(mysql_error(),E_USER_ERROR);}
+$topass['message']['type']=MESSAGE_INFO;
+$topass['message']['text']=$GLOBALS['_lang'][275];
+redirect2page('info.php',$topass);
