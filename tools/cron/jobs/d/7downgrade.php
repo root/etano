8f5ad8578ccaab8e2c96a00be7c42a05ffc9e4fd
dan <dan@eeac7d0b-4535-ccf8-9bcc-215df3ce543c>
2008-01-23 10:59:31 +0000

    - some checks in the downgrade cron job and gmdate instead of now()

--- tools/cron/jobs/d/7downgrade.php
+++ tools/cron/jobs/d/7downgrade.php
@@ -4,8 +4,8 @@ $jobs[]='downgrade';
 function downgrade() {
 	global $dbtable_prefix;
 
-	$now=gmdate('YmdHis');
-	$query="SELECT `payment_id`,`fk_user_id` FROM `{$dbtable_prefix}payments` WHERE `paid_until`<=curtime() AND `is_active`=1 AND `is_subscr`=1 AND `paid_until`<>'0000-00-00'";
+	$now=gmdate('Ymd');
+	$query="SELECT `payment_id`,`fk_user_id` FROM `{$dbtable_prefix}payments` WHERE `paid_until`<='$now' AND `is_active`=1 AND `is_subscr`=1 AND `paid_until`<>'0000-00-00'";
 	if (!($res=@mysql_query($query))) {trigger_error(mysql_error(),E_USER_ERROR);}
 	$user_ids=array();
 	$payment_ids=array();
@@ -13,11 +13,14 @@ function downgrade() {
 		$payment_ids[]=$rsrow['payment_id'];
 		$user_ids[]=$rsrow['fk_user_id'];
 	}
-	$query="UPDATE `{$dbtable_prefix}payments` SET `is_active`=0 WHERE `payment_id` IN ('".join("','",$payment_ids)."')";
-	if (!($res=@mysql_query($query))) {trigger_error(mysql_error(),E_USER_ERROR);}
-	$query="UPDATE `".USER_ACCOUNTS_TABLE."` SET `membership`=2 WHERE `".USER_ACCOUNT_ID."` IN ('".join("','",$user_ids)."')";
-	if (!($res=@mysql_query($query))) {trigger_error(mysql_error(),E_USER_ERROR);}
-
+	if (!empty($payment_ids)) {
+		$query="UPDATE `{$dbtable_prefix}payments` SET `is_active`=0 WHERE `payment_id` IN ('".join("','",$payment_ids)."')";
+		if (!($res=@mysql_query($query))) {trigger_error(mysql_error(),E_USER_ERROR);}
+	}
+	if (!empty($user_ids)) {
+		$query="UPDATE `".USER_ACCOUNTS_TABLE."` SET `membership`=2 WHERE `".USER_ACCOUNT_ID."` IN ('".join("','",$user_ids)."')";
+		if (!($res=@mysql_query($query))) {trigger_error(mysql_error(),E_USER_ERROR);}
+	}
 	// it would be nice to send an email notification to these members.
 	return true;
 }
