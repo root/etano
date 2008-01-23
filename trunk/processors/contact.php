5e01830cf1992747a532158175a71a6da545841f
dan <dan@eeac7d0b-4535-ccf8-9bcc-215df3ce543c>
2008-01-23 11:49:01 +0000

    - cleanup in processors/contact.php

--- trunk/processors/contact.php
+++ trunk/processors/contact.php
@@ -14,7 +14,6 @@ Support at:                 http://www.datemill.com/forum
 require_once '../includes/common.inc.php';
 db_connect(_DBHOST_,_DBUSER_,_DBPASS_,_DBNAME_);
 require_once '../includes/user_functions.inc.php';
-require_once '../includes/tables/blog_comments.inc.php';
 require_once _BASEPATH_.'/skins_site/'.get_my_skin().'/lang/contact.inc.php';
 check_login_member('contact');
 
