fd0edaac55040ded4425aadbc9f6ac9a215f6d60
dan <dan@eeac7d0b-4535-ccf8-9bcc-215df3ce543c>
2008-01-28 21:35:25 +0000

    - allowed 0 duration for manually assigned memberships

--- admin/processors/membership_assign.php
+++ admin/processors/membership_assign.php
@@ -30,28 +30,28 @@ if ($_SERVER['REQUEST_METHOD']=='POST') {
 	$input['duration']=sanitize_and_format_gpc($_POST,'duration',TYPE_INT,0,0);
 	$input['return']=sanitize_and_format_gpc($_POST,'return',TYPE_STRING,$__field2format[FIELD_TEXTFIELD] | FORMAT_RUDECODE,'');
 
-	if (empty($input['duration'])) {
-		$error=true;
-		$topass['message']['type']=MESSAGE_ERROR;
-		$topass['message']['text']='Please enter the duration of this membership';
-	}
-
 	if (!$error) {
-		$query="UPDATE `{$dbtable_prefix}payments` SET `paid_until`=CURDATE(),`is_active`=0 WHERE `fk_user_id` IN ('".join("','",$input['uids'])."') AND `is_active`=1 AND `is_subscr`=1";
+		$now=gmdate('YmdHis');
+		$query="UPDATE `{$dbtable_prefix}payments` SET `paid_until`='$now',`is_active`=0 WHERE `fk_user_id` IN ('".join("','",$input['uids'])."') AND `is_active`=1 AND `is_subscr`=1";
 		if (!($res=@mysql_query($query))) {trigger_error(mysql_error(),E_USER_ERROR);}
 		$query="SELECT `".USER_ACCOUNT_ID."` as `user_id`,`".USER_ACCOUNT_USER."` as `user`,`membership` FROM `".USER_ACCOUNTS_TABLE."` WHERE `".USER_ACCOUNT_ID."` IN ('".join("','",$input['uids'])."')";
 		if (!($res=@mysql_query($query))) {trigger_error(mysql_error(),E_USER_ERROR);}
 		$uids=array();
 		$insert="INSERT INTO `{$dbtable_prefix}payments` (`is_active`,`is_subscr`,`fk_user_id`,`_user`,`gateway`,`m_value_to`,`paid_from`,`paid_until`,`date`) VALUES ";
 		$query=$insert;
-		$now=gmdate('YmdHis');
 		while ($rsrow=mysql_fetch_assoc($res)) {
 			if (strlen($query)>$query_strlen) {
 				$query=substr($query,0,-1);
 				if (!($res2=@mysql_query($query))) {trigger_error(mysql_error(),E_USER_ERROR);}
 				$query=$insert;
 			}
-			$query.="(1,1,'".$rsrow['user_id']."','".$rsrow['user']."','manual','".$input['m_value']."','$now','$now'+INTERVAL '".$input['duration']."' DAY,now()),";
+			$query.="(1,1,'".$rsrow['user_id']."','".$rsrow['user']."','manual','".$input['m_value']."','$now'";
+			if (!empty($input['duration'])) {
+				$query.=",'$now'+INTERVAL '".$input['duration']."' DAY";
+			} else {
+				$query.=",'0000-00-00'"
+			}
+			$query.=",now()),";
 		}
 		if ($query!=$insert) {
 			$query=substr($query,0,-1);
