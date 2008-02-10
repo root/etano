cdee3e6e941196c4f07ab313b4e37db55d135fa6
dan <dan@eeac7d0b-4535-ccf8-9bcc-215df3ce543c>
2008-02-10 21:36:19 +0000

    - added dynamic extra menu links in admin

--- trunk/admin/frame.php
+++ trunk/admin/frame.php
@@ -19,14 +19,22 @@ $message=isset($message) ? $message : (isset($topass['message']) ? $topass['mess
 if (!empty($message)) {
 	$message['type']=(!isset($message['type']) || $message['type']==MESSAGE_ERROR) ? 'message_error' : 'message_info';
 	if (is_array($message['text'])) {
-		$message['text']=join('<br>',$message['text']);
+		$message['text']=join('<br />',$message['text']);
 	}
 	$tpl->set_var('message',$message);
+}
+$query="SELECT `link_url`,`link_text` FROM `{$dbtable_prefix}admin_menu` WHERE `link_type`=".LINKTYPE_MAIN;
+if (!($res=@mysql_query($query))) {trigger_error(mysql_error(),E_USER_ERROR);}
+$menu_links=array();
+while ($rsrow=mysql_fetch_assoc($res)) {
+	$rsrow['link_text']=sanitize_and_format($rsrow['link_text'],TYPE_STRING,$__field2format[TEXT_DB2DISPLAY]);
+	$menu_links[]=$rsrow;
 }
 $tplvars['lk']=md5(_LICENSE_KEY_);
 $tplvars['bu']=rawurlencode(base64_encode(_BASEURL_));
 $tpl->set_var('tplvars',$tplvars);
-echo $tpl->process('','frame',TPL_FINISH | TPL_OPTIONAL | TPL_INCLUDE);
+$tpl->set_loop('menu_links',$menu_links);
+echo $tpl->process('','frame',TPL_FINISH | TPL_OPTIONAL | TPL_INCLUDE | TPL_LOOP);
 if (isset($_SESSION['topass'])) {
 	unset($_SESSION['topass']);
 }
