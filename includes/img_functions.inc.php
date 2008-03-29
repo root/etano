7633076ec771875135a15f5813607b461c2ba116
dan <dan@eeac7d0b-4535-ccf8-9bcc-215df3ce543c>
2008-03-29 15:46:33 +0000

    - added a fastimagecopyresampled function and bicubic resampling support in the thumbnail creation script.

--- includes/img_functions.inc.php
+++ includes/img_functions.inc.php
@@ -14,6 +14,8 @@ Support at:                 http://www.datemill.com/forum
 define('PAD_2SIDES',1);
 define('PAD_1SIDE',2);
 define('PAD_NONE',3);
+//define('BICUBIC_RESAMPLE',1);
+
 function save_thumbnail($image,$size,$save_path,$save_name,$config=array()) {
 	$myreturn=false;
 	$size=array($size,$size);
@@ -64,7 +66,11 @@ function save_thumbnail($image,$size,$save_path,$save_name,$config=array()) {
 			imagefilledrectangle($mynewimg,0,0,$size[0],$size[1],0xFFFFFF);
 			$x=(int)(($size[0]-$new_size[0])/2);
 			$y=(int)(($size[1]-$new_size[1])/2);
-			imagecopyresampled($mynewimg,$myimg,$x,$y,0,0,$new_size[0],$new_size[1],$orig_size[0],$orig_size[1]);
+			if (defined('BICUBIC_RESAMPLE')) {
+				imagecopyresamplebicubic($mynewimg,$myimg,$x,$y,0,0,$new_size[0],$new_size[1],$orig_size[0],$orig_size[1]);
+			} else {
+				fastimagecopyresampled($mynewimg,$myimg,$x,$y,0,0,$new_size[0],$new_size[1],$orig_size[0],$orig_size[1]);
+			}
 
 			if (!empty($config['watermark_text']) && function_exists('imagettftext')) {
 				$config['watermark_text_color']=str_pad($config['watermark_text_color'],6,'0',STR_PAD_RIGHT);
@@ -82,6 +88,17 @@ function save_thumbnail($image,$size,$save_path,$save_name,$config=array()) {
 				imagettftext($mynewimg,$font_size,0,$watermark_x,$watermark_y,$text_color2,_BASEPATH_.'/includes/fonts/arial.ttf',$config['watermark_text']);
 				//text second
 				imagettftext($mynewimg,$font_size,0,$watermark_x+1,$watermark_y+1,$text_color,_BASEPATH_.'/includes/fonts/arial.ttf',$config['watermark_text']);
+			}
+
+			if (!empty($config['watermark_image']) && is_file($config['watermark_image'])) {
+				$wm_image=@imagecreatefrompng($config['watermark_image']);
+				$wm_image_x=imagesx($wm_image);
+				$wm_image_y=imagesy($wm_image);
+				if (defined('BICUBIC_RESAMPLE')) {
+					imagecopyresamplebicubic($mynewimg,$wm_image,5,$new_size[1]-$wm_image_y-20,0,0,$wm_image_x,$wm_image_y,$wm_image_x,$wm_image_y);
+				} else {
+					fastimagecopyresampled($mynewimg,$wm_image,5,$new_size[1]-$wm_image_y-20,0,0,$wm_image_x,$wm_image_y,$wm_image_x,$wm_image_y);
+				}
 			}
 
 			if (!empty($config['round_corners'])) {
@@ -118,4 +135,71 @@ function save_thumbnail($image,$size,$save_path,$save_name,$config=array()) {
 		imagedestroy($mynewimg);
 	}
 	return $myreturn;
+}
+
+
+function imagecopyresamplebicubic($dst_img, $src_img, $dst_x, $dst_y, $src_x, $src_y, $dst_w, $dst_h, $src_w, $src_h) {
+	$scaleX=($src_w-1)/$dst_w;
+	$scaleY=($src_h-1)/$dst_h;
+	$scaleX2=$scaleX/2.0;
+	$scaleY2=$scaleY/2.0;
+	$tc=imageistruecolor($src_img);
+
+	for ($y=$src_y;$y<$src_y+$dst_h;$y++) {
+		$sY=$y*$scaleY;
+		$siY=(int)$sY;
+		$siY2=(int)$sY+$scaleY2;
+		for ($x=$src_x;$x<$src_x+$dst_w;$x++) {
+			$sX =$x*$scaleX;
+			$siX =(int)$sX;
+			$siX2=(int)$sX+$scaleX2;
+			if ($tc) {
+				$c1=imagecolorat($src_img, $siX, $siY2);
+				$c2=imagecolorat($src_img, $siX, $siY);
+				$c3=imagecolorat($src_img, $siX2, $siY2);
+				$c4=imagecolorat($src_img, $siX2, $siY);
+				$r=(($c1+$c2+$c3+$c4)>>2)&0xFF0000;
+				$g=((($c1&0xFF00)+($c2&0xFF00)+($c3&0xFF00)+($c4&0xFF00))>>2)&0xFF00;
+				$b=((($c1&0xFF)+($c2&0xFF)+($c3&0xFF)+($c4&0xFF))>>2);
+				imagesetpixel($dst_img, $dst_x+$x-$src_x, $dst_y+$y-$src_y, $r+$g+$b);
+			}  else {
+				$c1=imagecolorsforindex($src_img, imagecolorat($src_img,$siX,$siY2));
+				$c2=imagecolorsforindex($src_img, imagecolorat($src_img,$siX,$siY));
+				$c3=imagecolorsforindex($src_img, imagecolorat($src_img,$siX2,$siY2));
+				$c4=imagecolorsforindex($src_img, imagecolorat($src_img,$siX2,$siY));
+				$r=($c1['red']+$c2['red'] +$c3['red'] +$c4['red']  ) << 14;
+				$g=($c1['green']+$c2['green']+$c3['green']+$c4['green']) << 6;
+				$b=($c1['blue'] +$c2['blue'] +$c3['blue'] +$c4['blue'] ) >> 2;
+				imagesetpixel($dst_img, $dst_x+$x-$src_x, $dst_y+$y-$src_y, $r+$g+$b);
+			}
+		}
+	}
+}
+
+
+function fastimagecopyresampled(&$dst_image, $src_image, $dst_x, $dst_y, $src_x, $src_y, $dst_w, $dst_h, $src_w, $src_h, $quality = 3) {
+	// Plug-and-Play fastimagecopyresampled function replaces much slower imagecopyresampled.
+	// Just include this function and change all "imagecopyresampled" references to "fastimagecopyresampled".
+	// Typically from 30 to 60 times faster when reducing high resolution images down to thumbnail size using the default quality setting.
+	// Author: Tim Eckel - Date: 09/07/07 - Version: 1.1 - Project: FreeRingers.net - Freely distributable - These comments must remain.
+	//
+	// Optional "quality" parameter (defaults is 3). Fractional values are allowed, for example 1.5. Must be greater than zero.
+	// Between 0 and 1 = Fast, but mosaic results, closer to 0 increases the mosaic effect.
+	// 1 = Up to 350 times faster. Poor results, looks very similar to imagecopyresized.
+	// 2 = Up to 95 times faster.  Images appear a little sharp, some prefer this over a quality of 3.
+	// 3 = Up to 60 times faster.  Will give high quality smooth results very close to imagecopyresampled, just faster.
+	// 4 = Up to 25 times faster.  Almost identical to imagecopyresampled for most images.
+	// 5 = No speedup. Just uses imagecopyresampled, no advantage over imagecopyresampled.
+	if (empty($src_image) || empty($dst_image) || $quality <= 0) {
+		return false;
+	}
+	if ($quality<5 && (($dst_w*$quality)<$src_w || ($dst_h*$quality)<$src_h)) {
+		$temp=imagecreatetruecolor($dst_w*$quality+1,$dst_h*$quality+1);
+		imagecopyresized($temp, $src_image, 0, 0, $src_x, $src_y, $dst_w * $quality + 1, $dst_h * $quality + 1, $src_w, $src_h);
+		imagecopyresampled($dst_image, $temp, $dst_x, $dst_y, 0, 0, $dst_w, $dst_h, $dst_w * $quality, $dst_h * $quality);
+		imagedestroy($temp);
+	} else {
+		imagecopyresampled($dst_image, $src_image, $dst_x, $dst_y, $src_x, $src_y, $dst_w, $dst_h, $src_w, $src_h);
+	}
+	return true;
 }
