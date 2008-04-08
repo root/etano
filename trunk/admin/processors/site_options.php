d52e79e84651482e0311455ba8021ce9ee786d60
dan <dan@eeac7d0b-4535-ccf8-9bcc-215df3ce543c>
2008-04-08 18:37:56 +0000

    - added file uploads in site_options (especially for the watermark image).

--- trunk/admin/processors/site_options.php
+++ trunk/admin/processors/site_options.php
@@ -13,6 +13,7 @@ Support at:                 http://www.datemill.com/forum
 
 require_once '../../includes/common.inc.php';
 require_once '../../includes/admin_functions.inc.php';
+require_once '../../includes/classes/fileop.class.php';
 allow_dept(DEPT_ADMIN);
 
 $error=false;
@@ -22,7 +23,7 @@ $topass=array();
 if ($_SERVER['REQUEST_METHOD']=='POST') {
 	$input=array();
 
-	$query="SELECT `config_option`,`option_type`,`fk_module_code` FROM `{$dbtable_prefix}site_options3`";
+	$query="SELECT `config_option`,`option_type`,`fk_module_code`,`choices` FROM `{$dbtable_prefix}site_options3`";
 	if (!($res=@mysql_query($query))) {trigger_error(mysql_error(),E_USER_ERROR);}
 	while ($rsrow=mysql_fetch_assoc($res)) {
 		switch ($rsrow['option_type']) {
@@ -47,17 +48,34 @@ if ($_SERVER['REQUEST_METHOD']=='POST') {
 				$input[$rsrow['fk_module_code']][$rsrow['config_option']]=sanitize_and_format_gpc($_POST,$rsrow['fk_module_code'].'_'.$rsrow['config_option'],TYPE_INT,0,0);
 				break;
 
+			case FIELD_FILE:
+				if (!empty($_FILES[$rsrow['fk_module_code'].'_'.$rsrow['config_option']]['tmp_name'])) {
+					if ($temp=upload_file(_BASEPATH_.'/tmp',$rsrow['fk_module_code'].'_'.$rsrow['config_option'])) {
+						$f=new fileop();
+						if ($f->rename(_BASEPATH_.'/tmp/'.$temp,_BASEPATH_.$rsrow['choices'].'/'.$temp)) {
+							$input[$rsrow['fk_module_code']][$rsrow['config_option']]=_BASEPATH_.$rsrow['choices'].'/'.$temp;
+						} else {
+							$error=true;
+						}
+					} else {
+						$error=true;
+					}
+				}
+				break;
+
 		}
 	}
 
-	foreach ($input as $module_code=>$v) {
-		foreach ($v as $config_option=>$config_value) {
-			$query="UPDATE `{$dbtable_prefix}site_options3` SET `config_value`='$config_value' WHERE `config_option`='$config_option' AND `fk_module_code`='$module_code'";
-			if (!($res=@mysql_query($query))) {trigger_error(mysql_error(),E_USER_ERROR);}
+	if (!$error) {
+		foreach ($input as $module_code=>$v) {
+			foreach ($v as $config_option=>$config_value) {
+				$query="UPDATE `{$dbtable_prefix}site_options3` SET `config_value`='$config_value' WHERE `config_option`='$config_option' AND `fk_module_code`='$module_code'";
+				if (!($res=@mysql_query($query))) {trigger_error(mysql_error(),E_USER_ERROR);}
+			}
 		}
+		$topass['message']['type']=MESSAGE_INFO;
+		$topass['message']['text']='Site features updated.';
 	}
-	$topass['message']['type']=MESSAGE_INFO;
-	$topass['message']['text']='Site features updated.';
 
 	if (isset($_POST['module_code'])) {
 		$qs.=$qs_sep.'module_code='.$_POST['module_code'];
