cddf9f329987fcccd7e2fcb52ae0be9f2bd8f06a
dan <dan@eeac7d0b-4535-ccf8-9bcc-215df3ce543c>
2008-04-14 15:20:49 +0000

    - more changes related to comments_*

--- trunk/includes/tables/comments_blog.inc.php
+++ trunk/includes/tables/comments_blog.inc.php
@@ -2,8 +2,8 @@
 /******************************************************************************
 Etano
 ===============================================================================
-File:                       includes/tables/blog_comments.inc.php
-$Revision$
+File:                       includes/tables/comments_blog.inc.php
+$Revision: 207 $
 Software by:                DateMill (http://www.datemill.com)
 Copyright by:               DateMill (http://www.datemill.com)
 Support at:                 http://www.datemill.com/forum
@@ -11,5 +11,5 @@ Support at:                 http://www.datemill.com/forum
 * See the "docs/licenses/etano.txt" file for license.                         *
 ******************************************************************************/
 
-$blog_comments_default['defaults']=array('comment_id'=>0,'fk_parent_id'=>0,'fk_user_id'=>0,'comment'=>'');
-$blog_comments_default['types']=array('comment_id'=>FIELD_INT,'fk_parent_id'=>FIELD_INT,'fk_user_id'=>FIELD_INT,'comment'=>FIELD_TEXTAREA);
+$comments_blog_default['defaults']=array('comment_id'=>0,'fk_parent_id'=>0,'fk_user_id'=>0,'comment'=>'');
+$comments_blog_default['types']=array('comment_id'=>FIELD_INT,'fk_parent_id'=>FIELD_INT,'fk_user_id'=>FIELD_INT,'comment'=>FIELD_TEXTAREA);
