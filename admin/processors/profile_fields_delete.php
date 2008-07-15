760419e42d68af4986ddebdf3aaa811ee858d966
dan <dan@eeac7d0b-4535-ccf8-9bcc-215df3ce543c>
2008-07-15 19:39:36 +0000

    - fixed: profile fields are not deleted

--- admin/processors/profile_fields_delete.php
+++ admin/processors/profile_fields_delete.php
@@ -34,8 +34,10 @@ if (mysql_num_rows($res)) {
 	// the method below is VERY ugly and hardcoded but should do the trick for now.
 	if ($rsrow['field_type']=='field_select' || $rsrow['field_type']=='field_mchecks') {
 		$m=array();
-		preg_match_all("/\['lang'\]\[(\d)+?\]/",$rsrow['custom_config'],$m);
-		$key_ids=array_merge($key_ids,$m);
+		preg_match_all("/\['_lang'\]\[(\d+)\]/",$rsrow['custom_config'],$m);
+		$key_ids=array_merge($key_ids,$m[1]);
+		$query="DELETE FROM `{$dbtable_prefix}profile_field_accvals` WHERE `fk_pfield_id`=$pfield_id";
+		if (!($res=@mysql_query($query))) {trigger_error(mysql_error(),E_USER_ERROR);}
 	}
 	$query="DELETE FROM `{$dbtable_prefix}lang_strings` WHERE `fk_lk_id` IN ('".join("','",$key_ids)."')";
 	if (!($res=@mysql_query($query))) {trigger_error(mysql_error(),E_USER_ERROR);}
