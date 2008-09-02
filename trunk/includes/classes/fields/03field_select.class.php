2bc932ba07008e99a998c24475c79b65b2c7f3d5
dan <dan@eeac7d0b-4535-ccf8-9bcc-215df3ce543c>
2008-09-02 18:02:15 +0000

    - some changes to prevent problems with IE

--- trunk/includes/classes/fields/03field_select.class.php
+++ trunk/includes/classes/fields/03field_select.class.php
@@ -82,7 +82,7 @@ class field_select extends iprofile_field {
 				<a href="#" id="accvals_add_first" title="Add a new value at the beginning of the list">Add new value</a> (at the beginning of the list)
 				<div id="actual_values"></div>
 			</div>';
-			$query="SELECT a.`accval_id`,b.`lang_value` as `value`,a.`def_value`,a.`search_value` FROM `{$dbtable_prefix}profile_field_accvals` a, `{$dbtable_prefix}lang_strings` b WHERE a.`fk_lk_id_name`=b.`fk_lk_id` AND b.`skin`='$default_skin_code' AND a.`fk_pfield_id`=".$output['pfield_id']." ORDER BY a.`sort`";
+			$query="SELECT a.`accval_id`,b.`lang_value` as `valo`,a.`def_value`,a.`search_value` FROM `{$dbtable_prefix}profile_field_accvals` a, `{$dbtable_prefix}lang_strings` b WHERE a.`fk_lk_id_name`=b.`fk_lk_id` AND b.`skin`='$default_skin_code' AND a.`fk_pfield_id`=".$output['pfield_id']." ORDER BY a.`sort`";
 			if (!($res=@mysql_query($query))) {trigger_error(mysql_error(),E_USER_ERROR);}
 			$accvals=array();
 			// grab the accepted values and pass them to js in json format
@@ -114,7 +114,7 @@ class field_select extends iprofile_field {
 					$(\'#accvals_add_first\').click(function() {
 						var myval=prompt_value(\'\');
 						if (myval) {
-							my_accvals.add(0,{value:myval});
+							my_accvals.add(0,{valo:myval});
 							my_accvals.render(\'actual_values\');
 							rebind_events();
 						}
@@ -128,11 +128,11 @@ class field_select extends iprofile_field {
 					rebind_events();
 				});
 
-				function prompt_value(val) {
-					if (!val) {
-						val=\'\';
+				function prompt_value(valo) {
+					if (!valo) {
+						valo=\'\';
 					}
-					var myval=prompt(\'Please enter the new value.\',val);
+					var myval=prompt(\'Please enter the new value.\',valo);
 					if (myval && myval!=\'\') {
 						return myval;
 					}
@@ -142,9 +142,9 @@ class field_select extends iprofile_field {
 				function rebind_events() {
 					$(\'.accvals_edit\').click(function() {
 						var my_idx=$(this).attr(\'id\').substr(5);
-						var myval=prompt_value(my_accvals.container[my_idx].value);
+						var myval=prompt_value(my_accvals.container[my_idx].valo);
 						if (myval) {
-							my_accvals.change(my_idx,{value:myval});
+							my_accvals.change(my_idx,{valo:myval});
 							my_accvals.render(\'actual_values\');
 							rebind_events();
 						}
@@ -156,7 +156,7 @@ class field_select extends iprofile_field {
 						var myval=prompt_value(\'\');
 						if (myval) {
 							my_idx++;
-							my_accvals.add(my_idx,{value:myval});
+							my_accvals.add(my_idx,{valo:myval});
 							my_accvals.render(\'actual_values\');
 							rebind_events();
 						}
@@ -195,7 +195,7 @@ class field_select extends iprofile_field {
 			$json=new Services_JSON(SERVICES_JSON_SUPPRESS_ERRORS | SERVICES_JSON_LOOSE_TYPE);
 			$temp=$json->decode(urldecode(sanitize_and_format_gpc($_POST,'accvals_new',TYPE_STRING,0,'')));
 			for ($i=0;isset($temp[$i]);++$i) {
-				$temp[$i]['value']=sanitize_and_format($temp[$i]['value'],TYPE_STRING,$__field2format[FIELD_TEXTFIELD]);
+				$temp[$i]['valo']=sanitize_and_format($temp[$i]['valo'],TYPE_STRING,$__field2format[FIELD_TEXTFIELD]);
 				$temp[$i]['after']=(int)$temp[$i]['after'];
 				$temp[$i]['def_value']=(int)$temp[$i]['def_value'];
 				$temp[$i]['search_value']=(int)$temp[$i]['search_value'];
@@ -205,7 +205,7 @@ class field_select extends iprofile_field {
 			$temp=$json->decode(urldecode(sanitize_and_format_gpc($_POST,'accvals_changed',TYPE_STRING,0,'')));
 			for ($i=0;isset($temp[$i]);++$i) {
 				$temp[$i]['accval_id']=(int)$temp[$i]['accval_id'];
-				$temp[$i]['value']=sanitize_and_format($temp[$i]['value'],TYPE_STRING,$__field2format[FIELD_TEXTFIELD]);
+				$temp[$i]['valo']=sanitize_and_format($temp[$i]['valo'],TYPE_STRING,$__field2format[FIELD_TEXTFIELD]);
 				$temp[$i]['def_value']=(int)$temp[$i]['def_value'];
 				$temp[$i]['search_value']=(int)$temp[$i]['search_value'];
 			}
@@ -222,7 +222,7 @@ class field_select extends iprofile_field {
 					$query="INSERT INTO `{$dbtable_prefix}lang_keys` SET `lk_type`=".FIELD_TEXTFIELD.",`lk_diz`='Field value',`lk_use`=".LK_FIELD;
 					if (!($res=@mysql_query($query))) {trigger_error(mysql_error(),E_USER_ERROR);}
 					$accvals_new[$i]['fk_lk_id']=mysql_insert_id();
-					$query="INSERT INTO `{$dbtable_prefix}lang_strings` SET `lang_value`='".$accvals_new[$i]['value']."',`fk_lk_id`=".$accvals_new[$i]['fk_lk_id'].",`skin`='$default_skin_code'";
+					$query="INSERT INTO `{$dbtable_prefix}lang_strings` SET `lang_value`='".$accvals_new[$i]['valo']."',`fk_lk_id`=".$accvals_new[$i]['fk_lk_id'].",`skin`='$default_skin_code'";
 					if (!($res=@mysql_query($query))) {trigger_error(mysql_error(),E_USER_ERROR);}
 				}
 				// insert the new values and re-sort the values for this field. Ugly and slow code with lots of queries :(
@@ -271,7 +271,7 @@ class field_select extends iprofile_field {
 			if (!empty($accvals_changed)) {
 				for ($i=0;isset($accvals_changed[$i]);++$i) {
 					// update the language string
-					$query="UPDATE `{$dbtable_prefix}lang_strings` a,`{$dbtable_prefix}profile_field_accvals` b SET a.`lang_value`='".$accvals_changed[$i]['value']."' WHERE a.`fk_lk_id`=b.`fk_lk_id_name` AND a.`skin`='$default_skin_code' AND b.`accval_id`=".$accvals_changed[$i]['accval_id'];
+					$query="UPDATE `{$dbtable_prefix}lang_strings` a,`{$dbtable_prefix}profile_field_accvals` b SET a.`lang_value`='".$accvals_changed[$i]['valo']."' WHERE a.`fk_lk_id`=b.`fk_lk_id_name` AND a.`skin`='$default_skin_code' AND b.`accval_id`=".$accvals_changed[$i]['accval_id'];
 					if (!($res=@mysql_query($query))) {trigger_error(mysql_error(),E_USER_ERROR);}
 					// since this is a select box, there can be only one selected value.
 					// unselect all other values if this one is the selected one
