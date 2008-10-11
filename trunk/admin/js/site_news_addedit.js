7c8f1ef2b150171db4cf4def2f7bd0bda0e08c2e
dan <dan@eeac7d0b-4535-ccf8-9bcc-215df3ce543c>
2008-10-11 13:32:52 +0000

    - tinymce related js fixes

--- trunk/admin/js/site_news_addedit.js
+++ trunk/admin/js/site_news_addedit.js
@@ -8,9 +8,13 @@ $(function() {
 
 function check_form(the) {
 	tinyMCE.triggerSave(true,true);
-	var mycontent=tinyMCE.getContent();
-	if (mycontent=='') {
+	if (the.news_title.value=='') {
 		alert('Please enter the subject of the news');
+		return false;
+	}
+	var mycontent=tinyMCE.get('news_body').getContent();
+	if (mycontent=='') {
+		alert('Please enter the content of the news');
 		return false;
 	}
 	return true;
