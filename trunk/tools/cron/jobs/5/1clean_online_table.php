a52a31248da8c71d2618f1ad5de1b7268736ac71
dan <dan@eeac7d0b-4535-ccf8-9bcc-215df3ce543c>
2008-12-21 21:08:00 +0000

    - added the cleaning of visitors from the online table

--- trunk/tools/cron/jobs/5/1clean_online_table.php
+++ trunk/tools/cron/jobs/5/1clean_online_table.php
@@ -6,6 +6,9 @@ function clean_online_table() {
 	$config=get_site_option(array('inactive_time'),'core');
 
 	$now=gmdate('YmdHis');
+	$query="DELETE FROM `{$dbtable_prefix}online` WHERE `fk_user_id` IS NULL AND `last_activity`<'$now'-INTERVAL '".$config['inactive_time']."' MINUTE";
+	if (!($res=@mysql_query($query))) {trigger_error(mysql_error(),E_USER_ERROR);}
+
 	$query="SELECT a.`fk_user_id` FROM `{$dbtable_prefix}online` a WHERE a.`last_activity`<'$now'-INTERVAL '".$config['inactive_time']."' MINUTE AND `fk_user_id` IS NOT NULL";
 	if (!($res=@mysql_query($query))) {trigger_error(mysql_error(),E_USER_ERROR);}
 	$to_del=array();
