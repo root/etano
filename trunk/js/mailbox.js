2ea65065e5542a2fa08a8c4cec78ff7f149e150f
dan <dan@eeac7d0b-4535-ccf8-9bcc-215df3ce543c>
2009-03-29 16:27:58 +0000

    - updated some jquery deprecated code

--- trunk/js/mailbox.js
+++ trunk/js/mailbox.js
@@ -6,18 +6,18 @@ $(function() {
 		send_form('del');
 	});
 	$('#btn_move').bind('click',function() {
-		$('#mailbox_form input[@name=moveto_fid]').val($('#folder_id').val());
+		$('#mailbox_form input[name=moveto_fid]').val($('#folder_id').val());
 		send_form('move');
 	});
 });
 
 function send_form(new_act) {
-	$('#mailbox_form input[@name=act]').val(new_act);
+	$('#mailbox_form input[name=act]').val(new_act);
 	$('#mailbox_form')[0].submit();
 }
 
 function check_uncheck(is_checked,form_id,field_str) {
-	$('#'+form_id+' input[@type=checkbox]').each(function() {
+	$('#'+form_id+' input[type=checkbox]').each(function() {
 		if ($(this).attr('id').indexOf(field_str,0)==0) {
 			this.checked=is_checked;
 		}
