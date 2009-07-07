350347ec9c8a841385d680cbe11a3a1db10f964f
dan <dan@eeac7d0b-4535-ccf8-9bcc-215df3ce543c>
2009-07-07 13:18:02 +0000

    -added the ability to run individual cron jobs from cron_web.php

--- trunk/tools/cron/cron_web.php
+++ trunk/tools/cron/cron_web.php
@@ -22,124 +22,128 @@ ob_end_flush();
 if (isset($_SERVER['REMOTE_ADDR'])) {
 	$lk=sanitize_and_format_gpc($_GET,'lk',TYPE_STRING,$__field2format[FIELD_TEXTFIELD],'');
 	if ($lk==_LICENSE_KEY_) {
-		$day=(int)date('d');
-		$weekday=(int)date('w');	//0 for sunday
-		$hour=(int)date('H');
-		$minute=(int)date('i');
-		$minute=$minute-$minute%5;	// allow 4 minutes and 59 seconds run delay
+		if (!empty($_GET['run'])) {
+			include_once dirname(__FILE__).'/jobs/'.$_GET['run'];
+		} else {
+			$day=(int)date('d');
+			$weekday=(int)date('w');	//0 for sunday
+			$hour=(int)date('H');
+			$minute=(int)date('i');
+			$minute=$minute-$minute%5;	// allow 4 minutes and 59 seconds run delay
 
-print "time: $hour:$minute<br>";
+	print "time: $hour:$minute<br>";
 
-		$jobs=array();
-		// every 5 minutes
-		if ($minute%5==0) {
-			if ($dh=opendir(dirname(__FILE__).'/jobs/5')) {
-				while (($file=readdir($dh))!==false) {
-					if ($file{0}!='.' && substr($file,-3)=='php') {
-						include_once dirname(__FILE__).'/jobs/5/'.$file;
+			$jobs=array();
+			// every 5 minutes
+			if ($minute%5==0) {
+				if ($dh=opendir(dirname(__FILE__).'/jobs/5')) {
+					while (($file=readdir($dh))!==false) {
+						if ($file{0}!='.' && substr($file,-3)=='php') {
+							include_once dirname(__FILE__).'/jobs/5/'.$file;
+						}
 					}
+					closedir($dh);
 				}
-				closedir($dh);
 			}
-		}
 
-		// every 10 minutes
-		if ($minute%10==0) {
-			if ($dh=opendir(dirname(__FILE__).'/jobs/10')) {
-				while (($file=readdir($dh))!==false) {
-					if ($file{0}!='.' && substr($file,-3)=='php') {
-						include_once dirname(__FILE__).'/jobs/10/'.$file;
+			// every 10 minutes
+			if ($minute%10==0) {
+				if ($dh=opendir(dirname(__FILE__).'/jobs/10')) {
+					while (($file=readdir($dh))!==false) {
+						if ($file{0}!='.' && substr($file,-3)=='php') {
+							include_once dirname(__FILE__).'/jobs/10/'.$file;
+						}
 					}
+					closedir($dh);
 				}
-				closedir($dh);
 			}
-		}
 
-		// every 15 minutes
-		if ($minute%15==0) {
-			if ($dh=opendir(dirname(__FILE__).'/jobs/15')) {
-				while (($file=readdir($dh))!==false) {
-					if ($file{0}!='.' && substr($file,-3)=='php') {
-						include_once dirname(__FILE__).'/jobs/15/'.$file;
+			// every 15 minutes
+			if ($minute%15==0) {
+				if ($dh=opendir(dirname(__FILE__).'/jobs/15')) {
+					while (($file=readdir($dh))!==false) {
+						if ($file{0}!='.' && substr($file,-3)=='php') {
+							include_once dirname(__FILE__).'/jobs/15/'.$file;
+						}
 					}
+					closedir($dh);
 				}
-				closedir($dh);
 			}
-		}
 
-		// every 30 minutes
-		if ($minute%30==0) {
-			if ($dh=opendir(dirname(__FILE__).'/jobs/30')) {
-				while (($file=readdir($dh))!==false) {
-					if ($file{0}!='.' && substr($file,-3)=='php') {
-						include_once dirname(__FILE__).'/jobs/30/'.$file;
+			// every 30 minutes
+			if ($minute%30==0) {
+				if ($dh=opendir(dirname(__FILE__).'/jobs/30')) {
+					while (($file=readdir($dh))!==false) {
+						if ($file{0}!='.' && substr($file,-3)=='php') {
+							include_once dirname(__FILE__).'/jobs/30/'.$file;
+						}
 					}
+					closedir($dh);
 				}
-				closedir($dh);
 			}
-		}
 
-		// every 1 hour
-		if ($minute==55) {
-			if ($dh=opendir(dirname(__FILE__).'/jobs/60')) {
-				while (($file=readdir($dh))!==false) {
-					if ($file{0}!='.' && substr($file,-3)=='php') {
-						include_once dirname(__FILE__).'/jobs/60/'.$file;
+			// every 1 hour
+			if ($minute==55) {
+				if ($dh=opendir(dirname(__FILE__).'/jobs/60')) {
+					while (($file=readdir($dh))!==false) {
+						if ($file{0}!='.' && substr($file,-3)=='php') {
+							include_once dirname(__FILE__).'/jobs/60/'.$file;
+						}
 					}
+					closedir($dh);
 				}
-				closedir($dh);
 			}
-		}
 
-		// every 2 hours
-		if ($hour%2==0 && $minute==55) {
-			if ($dh=opendir(dirname(__FILE__).'/jobs/120')) {
-				while (($file=readdir($dh))!==false) {
-					if ($file{0}!='.' && substr($file,-3)=='php') {
-						include_once dirname(__FILE__).'/jobs/120/'.$file;
+			// every 2 hours
+			if ($hour%2==0 && $minute==55) {
+				if ($dh=opendir(dirname(__FILE__).'/jobs/120')) {
+					while (($file=readdir($dh))!==false) {
+						if ($file{0}!='.' && substr($file,-3)=='php') {
+							include_once dirname(__FILE__).'/jobs/120/'.$file;
+						}
 					}
+					closedir($dh);
 				}
-				closedir($dh);
 			}
-		}
 
-		// every day
-		// we want the minute 30 and not 55 because some jobs might read the current day and they should
-		// have some time to finish.
-		if ($hour==23 && $minute==30) {
-			if ($dh=opendir(dirname(__FILE__).'/jobs/d')) {
-				while (($file=readdir($dh))!==false) {
-					if ($file{0}!='.' && substr($file,-3)=='php') {
-						include_once dirname(__FILE__).'/jobs/d/'.$file;
+			// every day
+			// we want the minute 30 and not 55 because some jobs might read the current day and they should
+			// have some time to finish.
+			if ($hour==23 && $minute==30) {
+				if ($dh=opendir(dirname(__FILE__).'/jobs/d')) {
+					while (($file=readdir($dh))!==false) {
+						if ($file{0}!='.' && substr($file,-3)=='php') {
+							include_once dirname(__FILE__).'/jobs/d/'.$file;
+						}
 					}
+					closedir($dh);
 				}
-				closedir($dh);
 			}
-		}
 
-		// once per week: sunday 11:35 PM
-		// we want the minute 35 and not 55 because some jobs might read the current day and they should
-		// have some time to finish.
-		if ($weekday==0 && $hour==23 && $minute==35) {
-			if ($dh=opendir(dirname(__FILE__).'/jobs/w')) {
-				while (($file=readdir($dh))!==false) {
-					if ($file{0}!='.' && substr($file,-3)=='php') {
-						include_once dirname(__FILE__).'/jobs/w/'.$file;
+			// once per week: sunday 11:35 PM
+			// we want the minute 35 and not 55 because some jobs might read the current day and they should
+			// have some time to finish.
+			if ($weekday==0 && $hour==23 && $minute==35) {
+				if ($dh=opendir(dirname(__FILE__).'/jobs/w')) {
+					while (($file=readdir($dh))!==false) {
+						if ($file{0}!='.' && substr($file,-3)=='php') {
+							include_once dirname(__FILE__).'/jobs/w/'.$file;
+						}
 					}
+					closedir($dh);
 				}
-				closedir($dh);
 			}
-		}
 
-		// once per month: on 1st at 12:05 AM
-		if ($day==1 && $hour==0 && $minute==5) {
-			if ($dh=opendir(dirname(__FILE__).'/jobs/m')) {
-				while (($file=readdir($dh))!==false) {
-					if ($file{0}!='.' && substr($file,-3)=='php') {
-						include_once dirname(__FILE__).'/jobs/m/'.$file;
+			// once per month: on 1st at 12:05 AM
+			if ($day==1 && $hour==0 && $minute==5) {
+				if ($dh=opendir(dirname(__FILE__).'/jobs/m')) {
+					while (($file=readdir($dh))!==false) {
+						if ($file{0}!='.' && substr($file,-3)=='php') {
+							include_once dirname(__FILE__).'/jobs/m/'.$file;
+						}
 					}
+					closedir($dh);
 				}
-				closedir($dh);
 			}
 		}
 
