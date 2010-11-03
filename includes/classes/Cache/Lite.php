b70714bfe3fe11bedbe690ae1db1f7fd50eceee9
dancaragea <dancaragea@eeac7d0b-4535-ccf8-9bcc-215df3ce543c>
2010-11-03 13:18:24 +0000

    - php5 compatibility fix

--- includes/classes/Cache/Lite.php
+++ includes/classes/Cache/Lite.php
@@ -715,8 +715,8 @@ class Cache_Lite
         if ($fp) {
             clearstatcache();
             $length = @filesize($this->_file);
-            $mqr = get_magic_quotes_runtime();
-            set_magic_quotes_runtime(0);
+//            $mqr = get_magic_quotes_runtime();
+//            set_magic_quotes_runtime(0);
             if ($this->_readControl) {
                 $hashControl = @fread($fp, 32);
                 $length = $length - 32;
@@ -726,7 +726,7 @@ class Cache_Lite
             } else {
                 $data = '';
             }
-            set_magic_quotes_runtime($mqr);
+//            set_magic_quotes_runtime($mqr);
             if ($this->_fileLocking) @flock($fp, LOCK_UN);
             @fclose($fp);
             if ($this->_readControl) {
